a125e2e1ef2b549460791c5cd6242bb3664f0a1c
joaoPerez <joaopaulosistemas.86@gmail.com>
2020-06-24 22:30:40 -0300

    Update InterestRateIntegrationTests.cs
    More integrated tests

--- InterestRateApi/InterestRate.Tests/IntegrationTests/InterestRateIntegrationTests.cs
+++ InterestRateApi/InterestRate.Tests/IntegrationTests/InterestRateIntegrationTests.cs
@@ -9,24 +9,41 @@ using System.Threading.Tasks;
 
 namespace InterestRate.Tests.IntegrationTests
 {
-    [TestFixture]
-    class InterestRateIntegrationTests
+	[TestFixture]
+	class InterestRateIntegrationTests
 	{
-        private HttpClient _client;
+		private HttpClient _client;
 
-        [SetUp]
-        public void Setup()
-        {
-            WebApplicationFactory<Startup> webAppFactory = new WebApplicationFactory<Startup>();
-            _client = webAppFactory.CreateClient();
-        }
+		[SetUp]
+		public void Setup()
+		{
+			WebApplicationFactory<Startup> webAppFactory = new WebApplicationFactory<Startup>();
+			_client = webAppFactory.CreateClient();
+		}
 
-        [Test]
-        public async Task Get_WhenCalled_ReturnTheInterestRate()
-        {
-           var response = await _client.GetAsync("/InterestRate/taxaJuros");
+		[Test]
+		public async Task Get_WhenCalled_ReturnStatusOK()
+		{
+			var response = await _client.GetAsync("/taxaJuros");
+			response.EnsureSuccessStatusCode();
+		}
 
+		[Test]
+		public async Task Get_WhenCalled_IsNotNegative()
+		{
+			var response = await _client.GetAsync("/taxaJuros");
+			string value = await response.Content.ReadAsStringAsync();
 
-        }
-    }
+			Assert.GreaterOrEqual(float.Parse(value), 0);
+		}
+
+		[Test]
+		public async Task Get_WhenCalled_ReturnsFloat()
+		{
+			var response = await _client.GetAsync("/taxaJuros");
+			string value = await response.Content.ReadAsStringAsync();
+
+			Assert.IsTrue(float.TryParse(value, out _));
+		}
+	}
 }
